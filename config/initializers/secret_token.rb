40eca34f1bcf3554b8faf9fa4b0a31f07263f0b0
Zachary Tschirhart <ztschir@gmail.com>
2011-10-30 10:49:32 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KnudgeMe::Application.config.secret_token = 'f02d349613d704c59227d4f7766f0d9cae166321bd9585cb154944cbf063ebeac6d0c6bd33678bc0d590155f5bc8ca3d4d703733a716cc1c7502b4c50abecf03'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+KnudgeMe::Application.config.session_store :cookie_store, key: '_KnudgeMe_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# KnudgeMe::Application.config.session_store :active_record_store
